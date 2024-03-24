5c10249fc200524feb0a851cb1c35bf5ebc52755
Anica <anicanedeljkovic16@gmail.com>
2024-03-24 23:48:31 +0100

    lokalne i globalne promenljive

--- projects/numberguess-contracts/smart_contracts/numberguess/contract.py
+++ projects/numberguess-contracts/smart_contracts/numberguess/contract.py
@@ -2,6 +2,30 @@ import beaker
 import pyteal as pt
 
 
+class Ngstate:
+    number = beaker.LocalStateValue(
+        stack_type=pt.TealType.bytes, descr="Broj igraca", default=pt.Bytes("Bez broja")
+    )
+    hash_number = beaker.LocalStateValue(
+        stack_type=pt.TealType.bytes,
+        descr="Hash broj igraca",
+        default=pt.Bytes("Bez hash broja"),
+    )
+    prvi_igrac = beaker.GlobalStateValue(
+        stack_type=pt.TealType.bytes,
+        descr="Acc prvog igraca",
+        default=pt.Bytes("Bez igraca"),
+    )
+    drugi_igrac = beaker.GlobalStateValue(
+        stack_type=pt.TealType.bytes,
+        descr="Acc drugog igraca",
+        default=pt.Bytes("Bez igraca"),
+    )
+    betting_tip = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64, descr="Betting amount", default=pt.Int(0)
+    )
+
+
 app = beaker.Application("numberguess")
 
 
